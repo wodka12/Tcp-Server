190142c86a5ad3130e69e2761fbdfe9bd8eb1966
wodka12 <lichmond@gmail.com>
2016-05-26 18:47:36 +0900

    2016.05.26 rabbitamq add on

--- contextSwitch/Mysql.cpp
+++ contextSwitch/Mysql.cpp
@@ -2,16 +2,27 @@
 #include "Mysql.h"
 #include <mysql.h>
 
-//#pragma comment(lib, "libmysql.lib")
+//#pragma comment(lib, "lib_acl_cpp_vc2015d.lib")
+
 
 #define DB_HOST "localhost"
 #define DB_USER "root"
 #define DB_PASS "root"
 #define DB_NAME "battle_test"
 
+
+#include <BasicMessage.h>
+#include <Channel.h>
+
+#ifdef _MSC_VER
+# pragma warning ( push )
+# pragma warning ( disable: 4275 4251 )
+#endif
+
+//typedef boost::shared_ptr<CMysql> ptr_t;
+
 CMysql::CMysql()
 {
-	
 }
 
 
@@ -38,8 +49,40 @@ bool CMysql::initialize_mysql(void)
 	printf("[%s]_DB Connection OK\n", DB_NAME);
 
 	mysql_close(&mysql);
+
+
+
+	//AMQ TEST 2016.05.26
+	AmqpClient::Channel::ptr_t m_channel = AmqpClient::Channel::Create("localhost");
+	if (m_channel != NULL)
+	{
+		m_channel->DeclareQueue("1");
+		m_channel->BindQueue("alanqueue", "amq.direct", "alankey");
+
+		AmqpClient::BasicMessage::ptr_t msg_in = AmqpClient::BasicMessage::Create();
+
+		msg_in->Body("This is a test message.");
+
+		m_channel->BasicPublish("amq.direct", "alankey", msg_in);
+
+		//m_channel->BasicConsume("alanqueue", "consumertag");
+
+		//AmqpClient::BasicMessage::ptr_t msg_out = m_channel->BasicConsumeMessage("consumertag")->Message();
+
+	}
+	//AMQ TEST 2016.05.26
 #endif
 
+
+	/*
+	CoInitialize(NULL);
+
+	ICShapSamplePtr ptr(__uuidof(CShapSample));
+	ptr->ShowMessage();
+
+	ptr->Release();
+	CoUninitialize();
+	*/
 	return true;
 	//printf(mysql_get_client_info());
 }
